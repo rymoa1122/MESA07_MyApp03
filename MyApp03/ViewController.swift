60ffec38ae5b8d755792d658769b901a28be9a95
iii <iii@userde-MacBook-6.local>
2017-06-20 14:41:22 +0800

    v1

--- MyApp03/ViewController.swift
+++ MyApp03/ViewController.swift
@@ -8,11 +8,78 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var textinput: UITextField!
+    @IBOutlet weak var labelResult: UILabel!
+    @IBOutlet weak var textHistory: UITextView!
+    
+    private var stringAnswer:String?
+    private var counter = 0
+    
+    @IBAction func btnGuess(_ sender: Any) {
+        counter += 1
+        let stringInput = textinput.text!
+        
+        let stringResult = RongAPI.checkAB(answer: stringAnswer!,guess: stringInput)
+        labelResult.text = stringResult
+        
+        textHistory.text.append("\(counter).\(stringInput) => \(stringResult)\n")
+        textinput.text = ""
+        
+        if stringResult == "3A0B" {
+             showWinnerDialog()
+        }else if counter == 10 {
+             showLoserDialog()
+        }
+
+    }
+    
+    func showWinnerDialog() {
+        let alert:UIAlertController = UIAlertController(title: "遊戲結果", message: "恭喜老爺, 賀喜夫人", preferredStyle: UIAlertControllerStyle.alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: {
+            (action: UIAlertAction) -> Void in
+            self.initRound()
+        })
+        
+        alert.addAction(okAction)
+        
+        self.present(alert, animated: true, completion: nil)
+        
+    }
+    func showLoserDialog() {
+        let alert:UIAlertController = UIAlertController(title: "遊戲結果", message: "很遺憾\n答案是:\(stringAnswer!)", preferredStyle: UIAlertControllerStyle.alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: {
+            (action: UIAlertAction) -> Void in
+            self.initRound()
+        })
+        
+        alert.addAction(okAction)
+        
+        self.present(alert, animated: true, completion: nil)
+        
+    }
+    
+    private func initRound() {
+        textinput.text = ""
+        labelResult.text = "顯示結果"
+        textHistory.text = ""
+        stringAnswer = RongAPI.createAnswer(3)
+        counter = 0
+        textinput.resignFirstResponder()
+        
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+       btnGuess(self)
+        return true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       textinput.delegate = self
+        initRound()
     }
 
     override func didReceiveMemoryWarning() {
